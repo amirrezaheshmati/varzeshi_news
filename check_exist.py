029bda814bf8d7b89baffac3811444cb0025f49a
amirrezaheshmati <hshmtyamyrrda631@gmail.com>
2026-09-08 08:15:30 +0330

    sds

--- check_exist.py
+++ check_exist.py
@@ -1,17 +1,34 @@
+import requests
 import json
+import os
+
+
+GIST_TOKEN = os.environ["GIST_TOKEN"]
+GIST_ID = "179ddb3b89fc161ee3ebee5aeb999965"
 
 def check(url) :
-  with open("urls.json" , "r") as file :
-    urls = json.load(file)
-  
-  if url in urls :
-    return False
-  
-  if len(urls) >= 500 :
-    urls.pop(0)
-
-  else :
-    urls.append(url)
-    with open("urls.json" , "w") as file :
-      json.dump(urls , file)  
-    return True
+  r = requests.get(
+    f"https://api.github.com/gists/{GIST_ID}",
+    headers={
+      "Authorization": f"Bearer {GIST_TOKEN}"
+    }
+  )
+  data = r.json()
+  content = data["files"]["news.json"]["content"]
+  news = json.loads(content)
+  print(news)
+  # with open("urls.json" , "r") as file :
+    # urls = json.load(file)
+  # 
+  # if url in urls :
+    # return False
+  # 
+  # if len(urls) >= 500 :
+    # urls.pop(0)
+# 
+  # else :
+    # urls.append(url)
+    # with open("urls.json" , "w") as file :
+      # json.dump(urls , file)  
+    # return True
+#
