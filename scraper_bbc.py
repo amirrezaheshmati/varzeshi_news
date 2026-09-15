eb2cf4c7164b94090979fae305fed099483ec6fe
amirrezaheshmati <hshmtyamyrrda631@gmail.com>
2026-09-15 08:09:09 +0330

    u

--- scraper_bbc.py
+++ scraper_bbc.py
@@ -11,28 +11,36 @@ def read_news() :
       }
     )
     soup = BeautifulSoup(response.text, "html.parser")
-    for li in soup.find_all("li") :
-      a = li.find("a" , href = True)
-
-      if a :
-        link = a["href"]
-        if "https://www.bbc.com/persian/articles/" in link :
-          main_link = link.replace(".lite" , "")
-          response = requests.get(
-            main_link,
-            headers={
-                "User-Agent": "Mozilla/5.0"
-            }
-          )
-          soup = BeautifulSoup(response.text, "html.parser")
-          title = soup.select_one("h1").get_text()
-          image_url = soup.select_one("figure img")["src"]
-          summary = soup.select_one("b").get_text()
-          yield {
-            "title" : title,
-            "image_url" : image_url,
-            "summary" : summary,
-            "link" : main_link
-          }
+    links = filtered(soup)
+    print(len(links))
+    for link in links :
+      main_link = link.replace(".lite" , "")
+      response = requests.get(
+        main_link,
+        headers={
+            "User-Agent": "Mozilla/5.0"
+        }
+      )
+      soup = BeautifulSoup(response.text, "html.parser")
+      title = soup.select_one("h1").get_text()
+      image_url = soup.select_one("figure img")["src"]
+      summary = soup.select_one("b").get_text()
+      yield {
+        "title" : title,
+        "image_url" : image_url,
+        "summary" : summary,
+        "link" : main_link
+      }
   except :
     yield None
+
+
+def filtered(soup) :
+  links = []
+  for li in soup.find_all("li") :
+    a = li.find("a" , href = True)
+    if a :
+      link = a["href"]
+      if "https://www.bbc.com/persian/articles/" in link :
+        links.append(link)
+  return links
