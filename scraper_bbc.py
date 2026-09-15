e24c1700539b54c97e8e4972ac9e9aea76947a61
amirrezaheshmati <hshmtyamyrrda631@gmail.com>
2026-09-15 08:12:47 +0330

    g

--- scraper_bbc.py
+++ scraper_bbc.py
@@ -3,36 +3,33 @@ from bs4 import BeautifulSoup
 
 
 def read_news() :
-  try :
+  response = requests.get(
+    "https://www.bbc.com/persian/topics/cz7k02839xet.lite",
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    }
+  )
+  soup = BeautifulSoup(response.text, "html.parser")
+  links = filtered(soup)
+  print(len(links))
+  for link in links :
+    main_link = link.replace(".lite" , "")
     response = requests.get(
-      "https://www.bbc.com/persian/topics/cz7k02839xet.lite",
+      main_link,
       headers={
           "User-Agent": "Mozilla/5.0"
       }
     )
     soup = BeautifulSoup(response.text, "html.parser")
-    links = filtered(soup)
-    print(len(links))
-    for link in links :
-      main_link = link.replace(".lite" , "")
-      response = requests.get(
-        main_link,
-        headers={
-            "User-Agent": "Mozilla/5.0"
-        }
-      )
-      soup = BeautifulSoup(response.text, "html.parser")
-      title = soup.select_one("h1").get_text()
-      image_url = soup.select_one("figure img")["src"]
-      summary = soup.select_one("b").get_text()
-      yield {
-        "title" : title,
-        "image_url" : image_url,
-        "summary" : summary,
-        "link" : main_link
-      }
-  except :
-    yield None
+    title = soup.select_one("h1").get_text()
+    image_url = soup.select_one("figure img")["src"]
+    summary = soup.select_one("b").get_text()
+    yield {
+      "title" : title,
+      "image_url" : image_url,
+      "summary" : summary,
+      "link" : main_link
+    }
 
 
 def filtered(soup) :
